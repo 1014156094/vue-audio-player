bfc90b8f0bb5e5afd037f87df76eed642532581f
LiRiPeng <1014156094@qq.com>
2020-04-28 17:28:01 +0800

    feat: 配置 useBuiltIns: false

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    [
+      '@vue/app',
+      {
+        // 巨坑：默认情况下会使用 babel-polyfill，如果安装该 npm 的项目也使用了 babel-polyfill，则会报错，useBuiltIns：false 可以解决
+        // 详情看官网：https://cli.vuejs.org/zh/guide/browser-compatibility.html#polyfill
+        useBuiltIns: false
+      }
+    ]
   ]
 }
